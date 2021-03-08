9b4904c63c4efe24b4b514e0e09f51af7bc784c6
MaximeStOnge <31734216+MaximeStOnge@users.noreply.github.com>
2021-03-08 13:50:48 -0500

    Fix curl script

--- scripts/curlRequests.sh
+++ scripts/curlRequests.sh
@@ -2,7 +2,6 @@
 
 # A collection of simple curl requests that can be used to manually test endpoints before and while writing automated tests
 
-curl localhost:9090/queue
 curl localhost:9090/queue/42
-curl localhost:9090/queue -XPOST --header "Content-Type: application/json" -d '{"userid":13}'
-curl localhost:9090/delete/42
+curl localhost:9090/queue -XPOST --header "Content-Type: application/json" -d '{"userid":13, "userip":"123.123.123.123"}'
+curl localhost:9090/queue/42 -XDELETE
